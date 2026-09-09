c01cd3e465b31c628478c213f93f955dfe9c0673
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2026-09-09 13:56:16 -0300

    First commit: Estrutura inicial do Spring Boot e configuracao de seguranca

---
+++ src/test/java/br/org/sesisenai/estoqueapi/EstoqueapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.sesisenai.estoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
