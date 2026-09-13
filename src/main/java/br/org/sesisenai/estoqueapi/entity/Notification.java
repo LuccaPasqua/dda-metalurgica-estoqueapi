3c6640687ae6d170531aeb530415029d72aa9098
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2026-09-13 20:32:13 -0300

    fix(entity): add contructor and getters and setters for the Notification entity

--- src/main/java/br/org/sesisenai/estoqueapi/entity/Notification.java
+++ src/main/java/br/org/sesisenai/estoqueapi/entity/Notification.java
@@ -29,4 +29,47 @@ public class Notification {
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime sentAt;
+
+    public Notification() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Alert getAlert() {
+        return alert;
+    }
+
+    public void setAlert(Alert alert) {
+        this.alert = alert;
+    }
 }
