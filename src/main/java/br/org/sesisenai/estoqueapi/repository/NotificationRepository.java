1e4f1f3f9de61f8ac7d7615be121fc2e26761310
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2026-09-13 20:37:30 -0300

    feat(repositoy): notification repository

--- src/main/java/br/org/sesisenai/estoqueapi/repository/NotificationRepository.java
+++ src/main/java/br/org/sesisenai/estoqueapi/repository/NotificationRepository.java
@@ -1,4 +1,20 @@
 package br.org.sesisenai.estoqueapi.repository;
 
-public interface NotificationRepository {
+import br.org.sesisenai.estoqueapi.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUserIdOrderBySentAtDesc(Long userId);
+
+    List<Notification> findByUserIdAndReadFalse(Long userId);
+
+    long countByUserIdAndReadFalse(Long userId);
+
+    List<Notification> findBySentAtAfter(LocalDateTime sentAt);
 }
