a0b8e7ad763e194f92022407c2aa847ea7fbcce4
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2026-09-13 12:29:41 -0300

    feat(enums): adicionar enums de movimentacao e status

---
+++ src/main/java/br/org/sesisenai/estoqueapi/entity/StockMovement.java
@@ -0,0 +1,4 @@
+package br.org.sesisenai.estoqueapi.entity;
+
+public class StockMovement {
+}
---
+++ src/main/java/br/org/sesisenai/estoqueapi/enums/MotiveStockMovement.java
@@ -0,0 +1,8 @@
+package br.org.sesisenai.estoqueapi.enums;
+
+public enum MotiveStockMovement {
+    PURCHASE,
+    PRODUCTION_CONSUMPTION,
+    LOSS,
+    INVENTORY_ADJUSTMENT // Corrigido de INVENTARY para INVENTORY
+}
