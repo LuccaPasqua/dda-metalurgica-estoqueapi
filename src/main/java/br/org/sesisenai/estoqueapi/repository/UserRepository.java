9976f66ee09268f1c05369e1852bcdbe370746f0
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2026-09-13 21:21:41 -0300

    feat(repositoy): user request repository

--- src/main/java/br/org/sesisenai/estoqueapi/repository/UserRepository.java
+++ src/main/java/br/org/sesisenai/estoqueapi/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package br.org.sesisenai.estoqueapi.repository;
 
-public interface UserRepository {
+import br.org.sesisenai.estoqueapi.entity.User;
+import br.org.sesisenai.estoqueapi.enums.RoleUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmailC(String email);
+
+    List<User> findByNameContainingIgnoreCase(String name);
+    List<User> findByRole(RoleUser role);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
